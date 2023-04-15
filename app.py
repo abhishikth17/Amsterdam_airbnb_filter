99bd36dc5ed831f79704f52d68f7aa489760eb01
abhi <129564601+abhishikth17@users.noreply.github.com>
2023-04-14 22:37:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -90,10 +90,6 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-
-df = pd.read_csv(
-    "WK2_Airbnb_Amsterdam_listings_proj_solution.csv", index_col=0
-)
 st.dataframe(filter_dataframe(df))
 
 # Display dataframe and text
